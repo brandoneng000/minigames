78a7b7fd5188c9e9e08a24060c87748d6d39a750
Brandon Eng <brandoneng000@gmail.com>
2022-02-10 20:38:17 -0800

    Added player model

---
+++ user/player/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Player(models.Model):
+    won = models.PositiveIntegerField(default=0)
+    loss = models.PositiveIntegerField(default=0)
+    played = models.PositiveIntegerField(default=0)
